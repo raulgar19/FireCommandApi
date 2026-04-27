84670a731d5d21b4bc529a7b1fabee905e05df05
raulgar19 <raulgamu2003@gmail.com>
2026-04-27 18:49:24 +0200

    Integrate Azure Key Vault for DB connection string
    Added Azure Key Vault and Microsoft.Extensions.Azure dependencies. Configured SecretClient registration and updated DbContext to retrieve the SQL Server connection string from the "FireCommandDb" secret in Key Vault at runtime.

--- FireCommandApi/Program.cs
+++ FireCommandApi/Program.cs
@@ -1,9 +1,11 @@
+using Azure.Security.KeyVault.Secrets;
 using FireCommandApi.Data;
 using FireCommandApi.Repositories;
 using FireCommandApi.Services;
 using FireCommandModels.Repositories.Interfaces;
 using FireCommandModels.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Azure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,8 +25,18 @@ builder.Services.AddTransient<ICommunicationService, CommunicationService>();
 builder.Services.AddTransient<IDashboardService, DashboardService>();
 builder.Services.AddTransient<ITelegramService, TelegramService>();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("FireCommand")));
+builder.Services.AddAzureClients(factory =>
+{
+    factory.AddSecretClient(builder.Configuration.GetSection("KeyVault"));
+});
+
+builder.Services.AddDbContext<AppDbContext>((serviceProvider, options) =>
+{
+    var secretClient = serviceProvider.GetRequiredService<SecretClient>();
+    KeyVaultSecret secret = secretClient.GetSecret("FireCommandDb");
+
+    options.UseSqlServer(secret.Value);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
